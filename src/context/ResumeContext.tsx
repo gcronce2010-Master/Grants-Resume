94e862e153d00edb3fc9140130b685861345fded
Grant Cronce <gcronce2010@gmail.com>
2026-02-20 18:42:32 +0000

    Grant	Cronce Cloud	Platform	Job	Simulation Certificate	of	Completion Feb

--- src/context/ResumeContext.tsx
+++ src/context/ResumeContext.tsx
@@ -55,6 +55,13 @@ const initialResumeData: ResumeData = {
   ],
   education: [
     {
+      id: 'edu10',
+      institution: 'Forage',
+      degree: 'Cloud Platform Job Simulation',
+      startDate: 'Feb 2024',
+      endDate: 'Feb 2024',
+    },
+    {
       id: 'edu9',
       institution: 'Software Engineering Job Simulation',
       degree: 'Certificate of Completion',
